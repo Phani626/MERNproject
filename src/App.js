1113aec7bd8bf79982eeb82b8638bcb2148e1ef7
Phani <phani9018@gmail.com>
2022-10-01 21:47:12 +0530

    second commit with routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Users from "./users/pages/Users";
 
 function App() {
-  return <h1>Let's start!</h1>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Users />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
